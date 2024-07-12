d43f1e7960dcd6cffcdcf8814c33f043c46d018e
dongchyeon <kimdonghyun90766@gmail.com>
2024-07-12 13:19:13 +0900

    Style : 폰트, 생상 테마 적용

---
+++ lib/themes/color_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const Color primary = Color(0xFF1778FB);
+  static const Color primaryLight = Color(0x1A1681FF);
+  static const Color secondary = Color(0xFF0BA4B6);
+  static const Color secondaryLight = Color(0x1A0BA4B6);
+  static const Color secondary2 = Color(0xFF80C84B);
+  static const Color secondary2Light = Color(0x1A80C84B);
+  static const Color error = Color(0xFFFF5761);
+  static const Color errorLight = Color(0x1AFF5761);
+  static const Color errorLightFilled = Color(0xFFFFEEEF);
+
+  static const Color gray900 = Color(0xFF191F28);
+  static const Color gray800 = Color(0xFF333D4B);
+  static const Color gray700 = Color(0xFF4C5765);
+  static const Color gray600 = Color(0xFF6B7684);
+  static const Color gray500 = Color(0xFF8B95A1);
+  static const Color gray400 = Color(0xFFB0B8C1);
+  static const Color gray300 = Color(0xFFD1D6DB);
+  static const Color gray200 = Color(0xFFE5E8EB);
+  static const Color gray100 = Color(0xFFF2F4F6);
+  static const Color gray50 = Color(0xFFF9FAFB);
+  static const Color grayWhite = Color(0xFFFFFFFF);
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gyeonggi_express/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GyeonggiExpressApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
