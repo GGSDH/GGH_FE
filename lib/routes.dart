e22067f7e36bc73918d1458fa13d208e07852e17
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-07-17 12:47:16 +0900

    feat: Add SelectableGrid widget for customizable icon-based item selection

--- lib/routes.dart
+++ lib/routes.dart
@@ -7,83 +7,81 @@ import 'package:gyeonggi_express/ui/login/login_screen.dart';
 import 'package:gyeonggi_express/ui/mypage/mypage_screen.dart';
 import 'package:gyeonggi_express/ui/photobook/photobook_screen.dart';
 import 'package:gyeonggi_express/ui/recommend/recommend_screen.dart';
+import 'package:gyeonggi_express/ui/signup/signup_screen.dart';
 import 'package:gyeonggi_express/ui/splash/splash_screen.dart';
 
 enum Routes {
-  splash, login, home, recommend, photobook, mypage;
+  splash,
+  login,
+  signup,
+  home,
+  recommend,
+  photobook,
+  mypage;
 
-  static final GoRouter config = GoRouter(
-    initialLocation: '/',
-    observers: [RouterObserver()],
-    routes: [
-      GoRoute(
+  static final GoRouter config = GoRouter(initialLocation: '/', observers: [
+    RouterObserver()
+  ], routes: [
+    GoRoute(
         path: '/',
         name: Routes.splash.name,
-        builder: (context, state) => const SplashScreen()
-      ),
+        builder: (context, state) => const SplashScreen()),
 
-      // 여기에 회원가입/로그인 플로우 넣을 예정
-      ShellRoute(
+    // 여기에 회원가입/로그인 플로우 넣을 예정
+    ShellRoute(
         observers: [RouterObserver()],
         builder: (context, state, child) => Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(child: child)
-        ),
+            backgroundColor: Colors.white, body: SafeArea(child: child)),
         routes: [
           GoRoute(
-            path: '/login',
-            name: Routes.login.name,
-            builder: (context, state) => const LoginScreen()
-          )
-        ]
-      ),
+              path: '/login',
+              name: Routes.login.name,
+              builder: (context, state) => const LoginScreen()),
+          GoRoute(
+              path: "/signup",
+              name: Routes.signup.name,
+              builder: (context, state) => const SignupScreen())
+        ]),
 
-      // 탭 루트 화면들
-      ShellRoute(
+    // 탭 루트 화면들
+    ShellRoute(
         observers: [RouterObserver()],
         builder: (context, state, child) => Scaffold(
-          backgroundColor: Colors.white,
-          body: Scaffold(
-            body: child,
-            bottomNavigationBar: CustomBottomNavigationBar(
-              currentIndex: 0,
-              onTap: (index) {
-                if (index == 0) {
-                  context.go('/home');
-                } else if (index == 1) {
-                  context.go('/recommend');
-                } else if (index == 2) {
-                  context.go('/photobook');
-                } else if (index == 3) {
-                  context.go('/mypage');
-                }
-              },
-            ),
-          )
-        ),
+            backgroundColor: Colors.white,
+            body: Scaffold(
+              body: child,
+              bottomNavigationBar: CustomBottomNavigationBar(
+                currentIndex: 0,
+                onTap: (index) {
+                  if (index == 0) {
+                    context.go('/home');
+                  } else if (index == 1) {
+                    context.go('/recommend');
+                  } else if (index == 2) {
+                    context.go('/photobook');
+                  } else if (index == 3) {
+                    context.go('/mypage');
+                  }
+                },
+              ),
+            )),
         routes: [
           GoRoute(
-            path: '/home',
-            name: Routes.home.name,
-            builder: (context, state) => const HomeScreen()
-          ),
+              path: '/home',
+              name: Routes.home.name,
+              builder: (context, state) => const HomeScreen()),
           GoRoute(
-            path: '/recommend',
-            name: Routes.recommend.name,
-            builder: (context, state) => const RecommendScreen()
-          ),
+              path: '/recommend',
+              name: Routes.recommend.name,
+              builder: (context, state) => const RecommendScreen()),
           GoRoute(
-            path: '/photobook',
-            name: Routes.photobook.name,
-            builder: (context, state) => const PhotobookScreen()
-          ),
+              path: '/photobook',
+              name: Routes.photobook.name,
+              builder: (context, state) => const PhotobookScreen()),
           GoRoute(
-            path: '/mypage',
-            name: Routes.mypage.name,
-            builder: (context, state) => const MyPageScreen()
-          )
-        ]
-      )
-    ]
-  );
+              path: '/mypage',
+              name: Routes.mypage.name,
+              builder: (context, state) => const MyPageScreen())
+        ])
+  ]);
 }
