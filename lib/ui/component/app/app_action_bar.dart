915283de919a45f671bc2ae5e884b2faad323cb1
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-08-10 05:44:20 +0900

    refactor: refactor app action bar to support title

--- lib/ui/component/app/app_action_bar.dart
+++ lib/ui/component/app/app_action_bar.dart
@@ -4,13 +4,14 @@ import 'package:gyeonggi_express/themes/color_styles.dart';
 import 'package:gyeonggi_express/themes/text_styles.dart';
 
 class AppActionBar extends StatelessWidget {
+  final String? title;
   final String rightText;
   final VoidCallback onBackPressed;
-
   final List<ActionBarMenuItem> menuItems;
 
   const AppActionBar({
     super.key,
+    this.title,
     required this.rightText,
     required this.onBackPressed,
     required this.menuItems
@@ -22,43 +23,60 @@ class AppActionBar extends StatelessWidget {
       height: 56,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
           children: [
-            SizedBox(
-              width: 24,
-              child: IconButton(
-                icon: SvgPicture.asset(
-                  "assets/icons/ic_arrow_back.svg",
-                  width: 24,
-                  height: 24,
-                ),
-                onPressed: onBackPressed,
-                padding: EdgeInsets.zero
+            Positioned.fill(
+              child: Center(
+                child: title != null
+                    ? Text(
+                  title!,
+                  style: TextStyles.titleLarge.copyWith(fontWeight: FontWeight.w600, color: ColorStyles.gray900),
+                  overflow: TextOverflow.ellipsis,
+                )
+                    : const SizedBox(),
               ),
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Text(
-                    rightText,
-                    style: TextStyles.bodyLarge
-                        .copyWith(color: ColorStyles.gray400),
+                SizedBox(
+                  width: 24,
+                  child: IconButton(
+                      icon: SvgPicture.asset(
+                        "assets/icons/ic_arrow_back.svg",
+                        width: 24,
+                        height: 24,
+                      ),
+                      onPressed: onBackPressed,
+                      padding: EdgeInsets.zero
                   ),
                 ),
-                ...menuItems.map(
-                  (item) => Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: SizedBox(
-                      width: 24,
-                      child: IconButton(
-                        icon: item.icon,
-                        onPressed: item.onPressed,
-                        padding: EdgeInsets.zero
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (rightText.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        child: Text(
+                          rightText,
+                          style: TextStyles.bodyLarge
+                              .copyWith(color: ColorStyles.gray400),
+                        ),
+                      ),
+                    ...menuItems.map(
+                          (item) => Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: SizedBox(
+                          width: 24,
+                          child: IconButton(
+                              icon: item.icon,
+                              onPressed: item.onPressed,
+                              padding: EdgeInsets.zero
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ],
             ),
