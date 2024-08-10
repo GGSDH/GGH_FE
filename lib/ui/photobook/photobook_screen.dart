1b897c4eb273565c657bd573f0e365a9df07649e
dongchyeon <kimdonghyun90766@gmail.com>
2024-08-10 18:03:08 +0900

    Design : PhotoTicketItem 컴포넌트 작성

--- lib/ui/photobook/photobook_screen.dart
+++ lib/ui/photobook/photobook_screen.dart
@@ -11,6 +11,8 @@ import '../../themes/color_styles.dart';
 import '../../themes/text_styles.dart';
 import '../component/photobook/photobook_list_item.dart';
 
+import 'dart:math' as math;
+
 class PhotobookScreen extends StatefulWidget {
   const PhotobookScreen({super.key});
 
@@ -81,31 +83,34 @@ class _PhotobookScreenState extends State<PhotobookScreen> with RouteAware {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: SafeArea(
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                const _TabBarSection(),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      _PhotobookSection(
-                        mapControllerCompleter: _mapControllerCompleter,
-                        onAddPhotobook: () {
-                          GoRouter.of(context).go("${Routes.photobook.path}/${Routes.addPhotobook.path}");
-                        },
-                        showPhotobookList: _showBottomSheet,
-                      ),
-                      _PhotoTicketSection(),
-                    ],
+    return Material(
+      color: Colors.white,
+      child: DefaultTabController(
+        length: 2,
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const _TabBarSection(),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        _PhotobookSection(
+                          mapControllerCompleter: _mapControllerCompleter,
+                          onAddPhotobook: () {
+                            GoRouter.of(context).go("${Routes.photobook.path}/${Routes.addPhotobook.path}");
+                          },
+                          showPhotobookList: _showBottomSheet,
+                        ),
+                        _PhotoTicketSection(),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ]
+                ],
+              ),
+            ]
+          ),
         ),
       ),
     );
@@ -192,28 +197,28 @@ class _PhotobookSection extends StatelessWidget {
           ),
         ),
         Positioned(
-            bottom: 20,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              child: GestureDetector(
-                onTap: showPhotobookList,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: ColorStyles.primary,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Text(
-                    "목록 열기",
-                    style: TextStyles.titleSmall.copyWith(
-                      color: ColorStyles.grayWhite,
-                    ),
+          bottom: 20,
+          left: 0,
+          right: 0,
+          child: Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: showPhotobookList,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: ColorStyles.primary,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text(
+                  "목록 열기",
+                  style: TextStyles.titleSmall.copyWith(
+                    color: ColorStyles.grayWhite,
                   ),
                 ),
               ),
-            )
+            ),
+          )
         ),
       ]
     );
@@ -227,31 +232,214 @@ class _PhotoTicketSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: 300, // 높이 설정
-        child: PageView.builder(
-          controller: _controller,
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0), // 카드 간 간격 설정
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
-                  color: Colors.blue[(index + 1) * 100],
-                ),
-                child: Center(
-                  child: Text(
-                    'Page $index',
-                    style: const TextStyle(color: Colors.white, fontSize: 24),
+    return Stack(
+      children: [
+        Positioned(
+          bottom: -100,
+          child: SvgPicture.asset(
+            "assets/icons/ic_photo_ticket_illust.svg",
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover
+          ),
+        ),
+        Center(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: PhotoTicketItem(
+                    day: index + 1,
+                    date: "8월 ${index + 1}일",
+                    title: "너무 신나는 경기행 해커톤",
+                    location: "각자의 집",
                   ),
+                );
+              },
+            ),
+          ),
+        ),
+      ]
+    );
+  }
+}
+
+class PhotoTicketItem extends StatelessWidget {
+
+  const PhotoTicketItem({
+    required this.day,
+    required this.date,
+    required this.title,
+    required this.location,
+  });
+
+  final int day;
+  final String date;
+  final String title;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10), // Add padding to avoid clipping
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 28),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: ColorStyles.gray300,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: ColorStyles.gray300.withOpacity(0.5),
+                    blurRadius: 6,
+                    offset: const Offset(6, 6),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Day $day",
+                          style: const TextStyle(
+                            color: ColorStyles.gray900,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const Spacer(),
+                        Text(
+                          date,
+                          style: TextStyles.titleMedium.copyWith(
+                            color: ColorStyles.gray500,
+                          )
+                        )
+                      ]
+                    ),
+                    const SizedBox(height: 15),
+
+                    Expanded(
+                      child: Image.asset(
+                        "assets/images/img_dummy_place.png",
+                        width: double.infinity,
+                        height: 120,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    Text(
+                      title,
+                      style: TextStyles.titleSmall.copyWith(
+                        color: ColorStyles.gray900,
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          "assets/icons/ic_map_pin.svg",
+                          width: 14,
+                          height: 14,
+                          colorFilter: const ColorFilter.mode(
+                            ColorStyles.gray600,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          location,
+                          style: TextStyles.bodyMedium.copyWith(
+                            color: ColorStyles.gray800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 7,
+            child: Center(
+              child: CustomPaint(
+                size: const Size(25, 40),
+                painter: OffsetRectanglePainter(
+                  color: ColorStyles.primary
+                )
+              ),
+            ),
+          )
+        ]
       ),
     );
+  }
+}
+
+class OffsetRectanglePainter extends CustomPainter {
+  OffsetRectanglePainter({required this.color});
+
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = ColorStyles.primary
+      ..style = PaintingStyle.fill;
+
+    const radius = Radius.circular(4);  // Set radius to 4
+
+    final path = Path()
+      ..moveTo(0 + radius.x, 0) // Top left corner
+      ..lineTo(size.width - radius.x, 0) // Top right corner
+      ..arcToPoint(
+        Offset(size.width, radius.y),
+        radius: radius,
+        clockwise: true,
+      )
+      ..lineTo(size.width + 7, size.height - radius.y) // Bottom right side
+      ..arcToPoint(
+        Offset(size.width + 7 - radius.x, size.height),
+        radius: radius,
+        clockwise: true,
+      )
+      ..lineTo(7 + radius.x, size.height) // Bottom left side
+      ..arcToPoint(
+        Offset(7, size.height - radius.y),
+        radius: radius,
+        clockwise: true,
+      )
+      ..lineTo(0, radius.y) // Left side
+      ..arcToPoint(
+        Offset(0 + radius.x, 0),
+        radius: radius,
+        clockwise: true,
+      )
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
