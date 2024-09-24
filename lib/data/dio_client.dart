f9f4bb3a5d9a3b144a5ad7814046b9aea0b216dc
dongchyeon <kimdonghyun90766@gmail.com>
2024-09-24 23:00:46 +0900

    Fix : receiveTimeout 10초 -> 20초

--- lib/data/dio_client.dart
+++ lib/data/dio_client.dart
@@ -17,7 +17,7 @@ class DioClient {
       contentType: Headers.jsonContentType,
       baseUrl: Config.baseUrl,
       connectTimeout: const Duration(milliseconds: 5000),
-      receiveTimeout: const Duration(milliseconds: 10000),
+      receiveTimeout: const Duration(milliseconds: 20000),
     );
 
     _dio = Dio(options)
